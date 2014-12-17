9557b600ce8ae23676f1ff61f35a284e9744acd6
Chris Beaven <smileychris@gmail.com>
2014-12-18 09:42:54 +1300

    More generic virtualenvs completion, fix workon_home fallback

--- functions/cd.fish
+++ functions/cd.fish
@@ -1,6 +1,6 @@
 function cd
 	builtin cd $argv
-	set workon_home_or "$WORKON_HOME" "$HOME/.virtualenvs"
+	set workon_home_or $WORKON_HOME $HOME/.virtualenvs
 	set virtual_env "$workon_home_or[1]/"(basename $PWD)
 	set activation "$virtual_env/bin/activate.fish"
 	if test "$virtual_env" != "$VIRTUAL_ENV"
@@ -8,7 +8,7 @@ function cd
 			. $activation
 		else if begin
 				set project "$virtual_env/.project"
-			    test -f "$project"
+				test -f "$project"
 				and test "$PWD" = (cat $project) -a -f "$activation"
 			end
 			. $activation
